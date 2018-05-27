39a0e1b66d810448a36929ac0833eea10d9c31f2
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-05-27 21:46:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ class Student
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-    SELECT SUM(grade) IN students
+    SELECT SUM(grade) FROM students
     WHERE grade = 9
     SQL
     DB[:conn].execute(sql)
